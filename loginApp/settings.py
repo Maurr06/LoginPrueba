30db1e9d17c127524517b4dfa4eb2cb7df3b4384
MauroGITGlobal <maurrx1@gmail.com>
2024-11-28 17:05:13 -0300

    correcion con mauro

--- loginApp/settings.py
+++ loginApp/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/accounts/login'
-LOGIN_REDIRECT_URL = '/blog'
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/blog/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
